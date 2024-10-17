b87c1c188ccd6fba5d9c9ca4fed818c39974b595
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-18 00:06:44 +0530

    [vaibhav]added: Modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { BookListComponent } from './book-list/book-list.component';
 import { ViewBookComponent } from './view-book/view-book.component';
 import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
   {path: 'cart', component: CartComponent},
   { path: 'books', component: BookListComponent },
   { path: 'view-book/:id', component: ViewBookComponent }
