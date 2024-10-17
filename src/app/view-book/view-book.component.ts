301c51ce3a043895c6753cb5432f233caa83d5b9
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-18 00:08:16 +0530

    [vaibhav]added: modified

--- src/app/view-book/view-book.component.ts
+++ src/app/view-book/view-book.component.ts
@@ -1,6 +1,80 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { BookSharedService } from '../services/book-shared.service';
+
+// @Component({
+//   selector: 'app-view-book',
+//   templateUrl: './view-book.component.html',
+//   styleUrls: ['./view-book.component.scss']
+// })
+// export class ViewBookComponent implements OnInit {
+//   book: any;
+//   quantityToBuy: number = 1; 
+//   isInWishlist: boolean = false; 
+//   isInBag: boolean = false; 
+//   cart: any[] = []; 
+//   wishlist: any[] = []; 
+//   constructor(
+//     private bookSharedService: BookSharedService, 
+//     private router: Router
+//   ) { }
+
+//   ngOnInit(): void {
+//     this.book = this.bookSharedService.getBook();
+//     if (!this.book) {
+//       this.router.navigate(['/books']);
+//     } else {
+      
+//       this.isInWishlist = this.wishlist.some(item => item._id === this.book._id);
+//     }
+//   }
+
+//   addToBag(): void {
+//     const cartItem = {
+//       ...this.book,
+//       quantity: this.quantityToBuy 
+//     };
+    
+    
+//     const existingItem = this.cart.find(item => item._id === cartItem._id);
+//     if (existingItem) {
+//       existingItem.quantity += this.quantityToBuy; 
+//     } else {
+//       this.cart.push(cartItem); 
+//     }
+
+//     this.isInBag = true; 
+//     console.log('Cart:', this.cart); 
+//   }
+
+  
+//   toggleWishlist(): void {
+//     if (this.isInWishlist) {
+//       this.wishlist = this.wishlist.filter(item => item._id !== this.book._id);
+//     } else {
+//       this.wishlist.push(this.book);
+//     }
+//     this.isInWishlist = !this.isInWishlist; 
+//     console.log('Wishlist:', this.wishlist); 
+//   }
+
+  
+//   decrementQuantity(): void {
+//     if (this.quantityToBuy > 1) {
+//       this.quantityToBuy--;
+//     }
+//   }
+
+//   incrementQuantity(): void {
+//     this.quantityToBuy++;
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BookSharedService } from '../services/book-shared.service';
+import { CartWishlistService } from '../services/cartwishlist.service';
 
 @Component({
   selector: 'app-view-book',
@@ -9,56 +83,36 @@ import { BookSharedService } from '../services/book-shared.service';
 })
 export class ViewBookComponent implements OnInit {
   book: any;
-  quantityToBuy: number = 1; 
-  isInWishlist: boolean = false; 
-  isInBag: boolean = false; 
-  cart: any[] = []; 
-  wishlist: any[] = []; 
+  quantityToBuy: number = 1;
+  isInWishlist: boolean = false;
+  isInBag: boolean = false;
+
   constructor(
-    private bookSharedService: BookSharedService, 
+    private bookSharedService: BookSharedService,
+    private cartWishlistService: CartWishlistService, 
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.book = this.bookSharedService.getBook();
     if (!this.book) {
       this.router.navigate(['/books']);
     } else {
-      
-      this.isInWishlist = this.wishlist.some(item => item._id === this.book._id);
+      this.isInBag = this.cartWishlistService.isInCart(this.book._id);
+      this.isInWishlist = this.cartWishlistService.isInWishlist(this.book._id);
     }
   }
 
   addToBag(): void {
-    const cartItem = {
-      ...this.book,
-      quantity: this.quantityToBuy 
-    };
-    
-    
-    const existingItem = this.cart.find(item => item._id === cartItem._id);
-    if (existingItem) {
-      existingItem.quantity += this.quantityToBuy; 
-    } else {
-      this.cart.push(cartItem); 
-    }
-
-    this.isInBag = true; 
-    console.log('Cart:', this.cart); 
+    this.cartWishlistService.addToCart(this.book, this.quantityToBuy);
+    this.isInBag = true;
   }
 
-  
   toggleWishlist(): void {
-    if (this.isInWishlist) {
-      this.wishlist = this.wishlist.filter(item => item._id !== this.book._id);
-    } else {
-      this.wishlist.push(this.book);
-    }
-    this.isInWishlist = !this.isInWishlist; 
-    console.log('Wishlist:', this.wishlist); 
+    this.cartWishlistService.toggleWishlist(this.book);
+    this.isInWishlist = this.cartWishlistService.isInWishlist(this.book._id);
   }
 
-  
   decrementQuantity(): void {
     if (this.quantityToBuy > 1) {
       this.quantityToBuy--;
