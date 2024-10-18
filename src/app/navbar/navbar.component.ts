4a9945a6ee9fb665221a89ffc54ce99342da8ca6
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-18 17:52:37 +0530

    [vaibhav]added: Refactored

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BookSharedService } from '../services/book-shared.service';
+import { BookService } from '../services/book.service';
+import { CartWishlistService } from '../services/cartwishlist.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
-
-  constructor() { }
+export class NavbarComponent implements OnInit { 
+  cartItems:any[] = []
+  constructor(private bookService: BookService, private bookSharedService: BookSharedService, private cartWishlistService: CartWishlistService, private router: Router) {}
 
   ngOnInit(): void {
-  }
+    this.cartWishlistService.getCart().subscribe((cart) => {
+      this.cartItems = cart;
+      console.log('Cart Items: ', this.cartItems);
+    });
 
+    this.bookService.getBooks().subscribe((books) => {
+      this.bookSharedService.setBookList(books); 
+      console.log("Books in navbar: ", books);
+    });
+  }
+  goToCart(): void {
+    this.router.navigate(['/cart']);
+  }
 }
