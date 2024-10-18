2261f5292330e29730878f9ad7dcb6d360c038b2
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-18 17:50:34 +0530

    [vaibhav]added: cart functionality for getting book and use this book in template

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartWishlistService } from '../services/cartwishlist.service';
+import { BookSharedService } from '../services/book-shared.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+  cartItems: any[] = [];
+  booksInCart: any[] = [];
 
-  constructor() { }
+  constructor(
+    private cartWishlistService: CartWishlistService, 
+    private bookSharedService: BookSharedService
+  ) { }
 
   ngOnInit(): void {
-  }
+    this.cartWishlistService.getCart().subscribe((cart) => {
+      this.cartItems = cart;
+      console.log('Cart Items: ', this.cartItems);
+    });
 
+    this.bookSharedService.getBookList().subscribe((books) => {
+      this.booksInCart = books.filter((book) => this.cartItems.some((cartItem) => cartItem._id === book._id));
+    });
+  }
 }
+
