84d2bbbeba1bb8470b5a52431adf61f8c68bdb64
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-20 15:11:34 +0530

    [vaibhvav]added: refactored

--- src/app/view-book/view-book.component.ts
+++ src/app/view-book/view-book.component.ts
@@ -1,12 +1,86 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { BookSharedService } from '../services/book-shared.service';
+// import { CartWishlistService } from '../services/cartwishlist.service';
+// import { CartService } from '../services/cart-service/cart.service';
+// @Component({
+//   selector: 'app-view-book',
+//   templateUrl: './view-book.component.html',
+//   styleUrls: ['./view-book.component.scss']
+// })
+// export class ViewBookComponent implements OnInit {
+//   book: any;
+//   quantityToBuy: number = 1;
+//   isInWishlist: boolean = false;
+//   isInBag: boolean = false;
+
+//   constructor(
+//     private bookSharedService: BookSharedService,
+//     private cartWishlistService: CartWishlistService, 
+//     private router: Router,
+//     private cartService: CartService
+//   ) {}
+
+//   ngOnInit(): void {
+//     this.book = this.bookSharedService.getBook();
+//     if (!this.book) {
+//       this.router.navigate(['/books']);
+//     } else {
+//       this.isInBag = this.cartWishlistService.isInCart(this.book._id);
+//       this.isInWishlist = this.cartWishlistService.isInWishlist(this.book._id);
+//     }
+//   }
+
+//   addToBag(): void {
+//     this.cartWishlistService.addToCart(this.book, this.quantityToBuy); 
+//     this.isInBag = true;
+
+//     //actual backend call api
+//     this.cartService.addBookToCart(`cart/${this.book._id}`,this.book).subscribe({
+//         next: (res:any) => {
+//           console.log(res); 
+//         },
+//         error: (err:any) => {  
+//           console.log(err.message); 
+//         }
+//       });
+    
+//     //
+//     this.cartWishlistService.getCart().subscribe(cart => {
+//       console.log('Cart after adding to bag:', cart);
+//     });
+
+//   }
+
+//   toggleWishlist(): void {
+//     this.cartWishlistService.toggleWishlist(this.book);
+//     this.isInWishlist = this.cartWishlistService.isInWishlist(this.book._id);
+//   }
+
+//   decrementQuantity(): void {
+//     if (this.quantityToBuy > 1) {
+//       this.quantityToBuy--;
+//       this.cartWishlistService.updateQuantity(this.book._id, this.quantityToBuy);
+//     }
+//   }
+
+//   incrementQuantity(): void {
+//     this.quantityToBuy++;
+//     this.cartWishlistService.updateQuantity(this.book._id, this.quantityToBuy);
+//   }
+  
+// }
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BookSharedService } from '../services/book-shared.service';
 import { CartWishlistService } from '../services/cartwishlist.service';
+import { CartService } from '../services/cart-service/cart.service';
 
 @Component({
   selector: 'app-view-book',
   templateUrl: './view-book.component.html',
-  styleUrls: ['./view-book.component.scss']
+  styleUrls: ['./view-book.component.scss'],
 })
 export class ViewBookComponent implements OnInit {
   book: any;
@@ -16,8 +90,9 @@ export class ViewBookComponent implements OnInit {
 
   constructor(
     private bookSharedService: BookSharedService,
-    private cartWishlistService: CartWishlistService, 
-    private router: Router
+    private cartWishlistService: CartWishlistService,
+    private router: Router,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
@@ -31,8 +106,26 @@ export class ViewBookComponent implements OnInit {
   }
 
   addToBag(): void {
-    this.cartWishlistService.addToCart(this.book, this.quantityToBuy); 
-    this.isInBag = true;
+    const isLoggedIn = this.checkIfUserIsLoggedIn();
+    if (isLoggedIn) {
+      // Logged-in user: call backend API to add the book to the cart
+      this.cartService.addBookToCart(`cart/${this.book._id}`, this.book).subscribe({
+        next: (res: any) => {
+          console.log('Book added to cart (Logged in):', res);
+          this.isInBag = true;
+        },
+        error: (err: any) => {
+          console.error('Error adding book to cart:', err.message);
+        }
+      });
+    } else {
+      // Logged-out user: Use CartWishlistService to add to local cart
+      this.cartWishlistService.addToCart(this.book, this.quantityToBuy);
+      this.isInBag = true;
+      console.log('Book added to cart (Logged out):', this.book);
+    }
+
+    // For non-logged-in users: update the local cart
     this.cartWishlistService.getCart().subscribe(cart => {
       console.log('Cart after adding to bag:', cart);
     });
@@ -53,5 +146,10 @@ export class ViewBookComponent implements OnInit {
   incrementQuantity(): void {
     this.quantityToBuy++;
     this.cartWishlistService.updateQuantity(this.book._id, this.quantityToBuy);
+  }
+
+  // Helper method to check if the user is logged in
+  checkIfUserIsLoggedIn(): boolean {
+    return !!localStorage.getItem('accessToken');
   }
 }
