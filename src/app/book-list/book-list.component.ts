1a2a17595fe61ec903e3a48f733f18073f60ef88
Vaibhav <sukalevaibhav53@gmail.com>
2024-10-20 15:03:01 +0530

    [vaibhav]added: Refactored

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,43 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { BookService } from '../services/book.service';
-@Component({
-  selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
-})
-export class BookListComponent implements OnInit {
-  books: any[] = [];
-  constructor(private bookService: BookService) { }
-
-  ngOnInit(): void {
-    this.bookService.getBooks().subscribe({
-      next: (data) => {
-        this.books = data.data;
-        console.log('Books fetched successfully:', this.books);
-      },
-      error: (error) => {
-        console.error('Error fetching books:', error);
-      }
-    });
-  }
-
-}
-
 // import { Component, OnInit } from '@angular/core';
-// import { BookSharedService } from '../services/book-shared.service';
-
+// import { BookService } from '../services/book.service';
 // @Component({
 //   selector: 'app-book-list',
 //   templateUrl: './book-list.component.html',
-//   styleUrls: ['./book-list.component.scss'],
+//   styleUrls: ['./book-list.component.scss']
 // })
 // export class BookListComponent implements OnInit {
 //   books: any[] = [];
-
-//   constructor(private bookSharedService: BookSharedService) {}
+//   constructor(private bookService: BookService) { }
 
 //   ngOnInit(): void {
-//     // Fetch the book list from BookSharedService
-//     this.books = this.bookSharedService.getBookList();
+//     this.bookService.getBooks().subscribe({
+//       next: (data) => {
+//         this.books = data.data;
+//         console.log('Books fetched successfully:', this.books);
+//       },
+//       error: (error) => {
+//         console.error('Error fetching books:', error);
+//       }
+//     });
 //   }
+
 // }
+
+
+//Use above code 
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../services/book-service/book.service';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss'],
+})
+export class BookListComponent implements OnInit {
+  books: any[] = [];
+
+  constructor(private bookesService: BookService) {}
+
+  ngOnInit(): void {
+    this.loadBookes();
+  }
+
+  loadBookes(){
+    this.bookesService.getBooksApiCall('books').subscribe({
+      next: (res: any)=>{
+        this.books = res.data;
+      },
+      error: (err)=>{
+        console.log(err);
+      },
+    });
+  }
+}
